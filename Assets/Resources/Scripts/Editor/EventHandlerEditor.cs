7e0c62acc5d2b991df32b222b1ff95a15ac7c662
Unknown <e_q101@live.com>
2017-08-29 10:55:02 -0500

    Auto stash before merge of "ekgrahn-working" and "ndcregut-working"

---
+++ Assets/Resources/Scripts/Editor/EventHandlerEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EventHandler))]
+[CanEditMultipleObjects]
+public class EventHandlerEditor : Editor {
+
+    EventHandler eh;
+
+    void OnEnable()
+    {
+        eh = (EventHandler)target;   
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.Space();
+        EditorGUILayout.EnumPopup("Event Type", eh.eventType);
+        EditorGUILayout.Space();
+
+        switch (eh.eventType)
+        {
+            case EventHandler.EventType.TEXT:
+                EditorGUILayout.LabelField("Text To Display");
+                EditorGUILayout.TextField(string.Empty, GUILayout.Height(150));
+                EditorGUILayout.Space();
+                break;
+            case EventHandler.EventType.IMAGE:
+                EditorGUILayout.LabelField("Image To Display");
+                break;
+            case EventHandler.EventType.CUTSCENE:
+                break;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/EventHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventHandler : MonoBehaviour {
+
+    public enum EventType
+    {
+        TEXT,
+        IMAGE,
+        CUTSCENE
+    }
+    public EventType eventType = EventType.TEXT;
+    
+    public string textToDisplay;
+    public Image imageToDisplay;
+    public Object sceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Unity on collision enter event to trigger custom event.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    /// <summary>
+    /// Display text to player.
+    /// </summary>
+    private void DisplayText()
+    {
+        if (textToDisplay == string.Empty)
+            return;
+    }
+
+    /// <summary>
+    /// Display an image to the player.
+    /// </summary>
+    private void DisplayImage()
+    {
+        if (!imageToDisplay)
+            return;
+    }
+
+    /// <summary>
+    /// Load a scene that operates as a cutscene.
+    /// </summary>
+    private void LoadCutscene()
+    {
+        try
+        {
+            GameManager.Instance.SceneSwitch.ChangeLevel(sceneToLoad.name);
+        } catch
+        {
+            Debug.LogWarning("Object with name '" + sceneToLoad.name + "' may not be a scene.");
+        }
+    }
+}
