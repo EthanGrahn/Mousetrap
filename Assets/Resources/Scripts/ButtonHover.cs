67915db10a60f5a026f39641d545f26392e2241d
Unknown <e_q101@live.com>
2017-09-05 12:13:51 -0500

    Button Hover Rework
    Modified the button hover script so it doesn't scale. This way the text objects can still be anchored to the button without skewing.

--- Assets/Resources/Scripts/ButtonHover.cs
+++ Assets/Resources/Scripts/ButtonHover.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public class ButtonHover : MonoBehaviour {
 
-    public float transitionSpeed = 0.5f;
+    [SerializeField]
+    private float extentionAmt = 80;
+    [SerializeField]
+    private float transitionSpeed = 0.5f;
     [SerializeField]
     private AnimationCurve transitionCurve;
     private RectTransform rTransform;
+    private float initOffset;
+    private float scaleTime = 0;
 
     private void Start()
     {
         rTransform = GetComponent<RectTransform>();
         transitionCurve.MoveKey(1, new Keyframe(transitionSpeed, 1));
+        initOffset = rTransform.offsetMin.x;
     }
 
     /// <summary>
@@ -41,13 +47,14 @@ public class ButtonHover : MonoBehaviour {
     private IEnumerator Extend()
     {
         float ratio = 0;
-        for (float i = rTransform.localScale.x - 1; i <= transitionSpeed; i += Time.deltaTime)
+        for (float i = scaleTime; i <= transitionSpeed; i += Time.deltaTime)
         {
             yield return null;
-            ratio = transitionCurve.Evaluate(i);
-            rTransform.localScale = new Vector3(1 + (0.5f * ratio), 1f);
+            ratio = transitionCurve.Evaluate(i) * extentionAmt;
+            rTransform.offsetMin = new Vector2(initOffset - ratio, rTransform.offsetMin.y);
+            scaleTime = i;
         }
-        rTransform.localScale = new Vector3(1.5f, 1f);
+        rTransform.offsetMin = new Vector2(initOffset - extentionAmt, rTransform.offsetMin.y);
     }
 
     /// <summary>
@@ -56,14 +63,14 @@ public class ButtonHover : MonoBehaviour {
     /// <returns></returns>
     private IEnumerator Shrink()
     {
-        float scaleDiff = rTransform.localScale.x - 1;
         float ratio = 0;
-        for (float i = scaleDiff; i > 0; i -= Time.deltaTime)
+        for (float i = scaleTime; i > 0; i -= Time.deltaTime)
         {
             yield return null;
-            ratio = transitionCurve.Evaluate(i);
-            rTransform.localScale = new Vector3(1 + (0.5f * ratio), 1f);
+            ratio = transitionCurve.Evaluate(i) * extentionAmt;
+            rTransform.offsetMin = new Vector2(initOffset - ratio, rTransform.offsetMin.y);
+            scaleTime = i;
         }
-        rTransform.localScale = new Vector3(1f, 1f);
+        rTransform.offsetMin = new Vector2(initOffset, rTransform.offsetMin.y);
     }
 }
