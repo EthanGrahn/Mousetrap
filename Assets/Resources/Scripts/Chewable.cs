5214cbd6dcd1f1b7cb12695b604db8e7db4621e1
Unknown <e_q101@live.com>
2017-10-31 11:24:03 -0500

    Added initial chewing ability

---
+++ Assets/Resources/Scripts/Chewable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chewable : MonoBehaviour {
+
+    int mashes;
+    int mashCount = 0;
+
+    // Use this for initialization
+    void Start () {
+        mashes = Random.Range(8, 16);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Rebind.GetInputDown("Interact"))
+            mashCount++;
+
+        if (mashCount >= mashes)
+            Destroy(this.gameObject);
+    }
+}
